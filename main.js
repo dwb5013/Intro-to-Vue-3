532f93b9b457a9d43ba1579fad2350948c9a9732
digika-ding <weibing.ding@digika.jp>
2023-11-10 23:08:40 +0900

    my-l8

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
+            cart: 0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+            variants: [{id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50}, {
+                id: 2235,
+                color: 'blue',
+                image: './assets/images/socks_blue.jpg',
+                quantity: 0
+            },],
+            selectedVariant: 0,
+            onSale: true
         }
-    },
-    methods: {
+    }, methods: {
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariants(index) {
+            this.selectedVariant = index
+        }
+    }, computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        sale(){
+            return this.onSale ? this.brand + ' ' + this.product + ' is on sale!' : ''
         }
     }
 })
